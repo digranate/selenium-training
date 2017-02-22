ac8e68dc1c2721e747cc3a0d65b4fd7e2a9e67be
digranate <dbarshev1@googlemail.com>
2017-02-22 20:35:32 +0300

    add login scenario

--- scenario/src/test/java/LoginTest.java
+++ scenario/src/test/java/LoginTest.java
@@ -1,6 +1,7 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,19 +9,22 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 /**
  * Created by dinara.trifanova on 2/21/2017.
  */
-public class OpenPageTest {
+public class LoginTest {
     private WebDriver driver;
     private WebDriverWait webDriverWait;
 
     @Before
     public void beforeActions() {
-      driver = new ChromeDriver();
-      webDriverWait = new WebDriverWait(driver,10);
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void openPage() {
-        driver.get("http://yandex.ru");
+    public void login() {
+        driver.get("http://localhost:8080/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
